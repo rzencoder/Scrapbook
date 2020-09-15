286542b35692d72cb8dabd9b32cc8261f11ac41a
adam <20361733+rzencoder@users.noreply.github.com>
2020-09-15 16:10:16 +0100

    heroku deploy

--- server.js
+++ server.js
@@ -15,8 +15,8 @@ const authRoutes = require("./server/routes/auth");
 const cors = require("cors");
 
 const app = express();
-// const compiler = webpack(config);
-app.use(cors);
+// // const compiler = webpack(config);
+// app.use(cors);
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true }, function (
   err
 ) {
